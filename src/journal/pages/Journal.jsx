abd6d151915f98461e3b8ac1e9712b7a6130366c
Angel <gabrielcardozo3579@gmail.com>
2025-03-14 11:57:28 -0400

    feat: add ImagesGallery component and implement floating action button in Journal page

--- src/journal/pages/Journal.jsx
+++ src/journal/pages/Journal.jsx
@@ -1,14 +1,28 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView } from "../views/NothingSelectedView";
 import { NoteView } from "../views/NoteView";
+import { AddOutlined } from "@mui/icons-material";
 
 export const Journal = () => {
   return (
     <>
       <JournalLayout>
         <NothingSelectedView />
-        <NoteView />
+        {/* <NoteView /> */}
+        <IconButton
+          size="large"
+          sx={{
+            color: "white",
+            backgroundColor: "error.main",
+            ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+            position: "fixed",
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
       </JournalLayout>
     </>
   );
